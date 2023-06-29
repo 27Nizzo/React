29efa840f4e13a73a4199046a6a5c4a1bdee428c
Nizzo07 <afonso7557@gmail.com>
2023-06-29 17:08:31 +0100

    Video 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
 
 function App() {
+  const age2 = 17;
+  const isGreen = true;
+  const users = [
+    { name: "Pedro", age: 21},
+    { name: "Jake", age: 25 },
+    {name: "Jessica", age: 45}
+    
+  ];
 
   return <div className="App">
+
+    <div><h1>VIDEO 1 E 2</h1></div>
 
     <div>
       <User name = "Afonso" age = {19} mail = "afonso.martins8282@gmail.com"/>
@@ -10,7 +20,6 @@ function App() {
       <User name = "Nicole" age = {56} mail = "nicolecampos@gmail.com"/>
     </div>
 
-    
     <div>  
       <Job salary = {1200} position = "Junior Dev" company = "Meta"/>
       <Job salary = {3400} position = "Senior Dev" company = "Amazon" />
@@ -21,8 +30,26 @@ function App() {
       <Menu titulo = "Prato 1" prato = "Bacalhau à Brás" preço = {"12$"}  />
       <Menu titulo = "Prato 2" prato = "Massa à Bolonhesa" preço = {"15$"} />
       <Menu titulo = "Prato 3" prato = "Salmão Com Arroz" preço = {"18$"}/>
+    </div> 
+  
+  <div>------------------------------------------------------------------------------------------</div>
+
+  <div><h1>VIDEO 3</h1></div>
+    <div>
+      <h1 className = "name2">Afonso</h1>
     </div>
 
+  <div className ="App">
+    {age2 >= 18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
+    <h1 style = {{color: isGreen ? "green" : "red"}}>This has Color</h1>
+    {isGreen && <button><h2><em>THIS IS A BUTTON</em></h2></button>}
+  </div>
+  <div>-------------------------------------------------------------------------------------------</div>
+  <div className = "App">
+      <h1>{users.map((user, key) => {
+        return <div key = {key}>{user.name} {user.age}</div>
+      })}</h1>
+  </div>
   </div>
 }
 
@@ -68,6 +95,15 @@ const Menu = (props2) => {
     </div>
     
   )
+//----
+const Users1 = (props3) => {
+  return (
+    <div>
+      {props3.name} {props3.age}
+    </div>
+  )  
+}
+
 }
 
 
