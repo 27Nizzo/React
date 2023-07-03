b53d237156681bdafd86afb9c1e06101f2221c59
Nizzo07 <afonso7557@gmail.com>
2023-07-03 19:16:51 +0100

    Fim de Video 3

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
 import './App.css';
 
 function App() {
-  const users = [
-    {name: "Pedro", age: 21},
-    {name: "Ana", age: 20},
-    {name: "Jessica", age: 24},
-  ]
+ 
+  const planets = [
+    {name: "Mars", isGasPlanet: false},
+    {name: "Earth", isGasPlanet: false},
+    {name: "Jupiter", isGasPlanet: true},
+    {name: "Venus", isGasPlanet: false},
+    {name: "Neptune", isGasPlanet: true},
+    {name: "Uranus", isGasPlanet: true},
+  ];
 
 
   return <div className="App">
- 
-      {users.map((user, key) => {
-        return <div>
-          <User name = {user.name} age = {user.age} />
-        </div>
-      })}
-      
+    <div>
+      {planets.map(
+        (planet, key) => !planet.isGasPlanet && <h1>{planet.name}</h1>
+      )}
     </div>
 
-    
-    
- }
- const User = (props) => {
-  return (
-    <div>
-        <h1>{props.name} {props.age}</h1>
-    </div>
-  )
- }
+  </div>
  
+}
 
 export default App;
