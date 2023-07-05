4d8a803661e276d5b4f82a1581023eafe2e5b68f
Nizzo07 <afonso7557@gmail.com>
2023-07-05 18:50:03 +0100

    video 4 Pedro Tech

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import './App.css';
+import { useState } from "react"
 
 function App() {
- 
-  const planets = [
-    {name: "Mars", isGasPlanet: false},
-    {name: "Earth", isGasPlanet: false},
-    {name: "Jupiter", isGasPlanet: true},
-    {name: "Venus", isGasPlanet: false},
-    {name: "Neptune", isGasPlanet: true},
-    {name: "Uranus", isGasPlanet: true},
-  ];
+  const [count, setCount] = useState(0)
 
+  const increase = () => {
+    setCount(count + 1)
+  }
+  
+  const decrease = () => {
+    setCount(count - 1)
+  }
 
+  const setTo0 = () => {
+    setCount(0)
+  }
+  
   return <div className="App">
     <div>
-      {planets.map(
-        (planet, key) => !planet.isGasPlanet && <h1>{planet.name}</h1>
-      )}
+      {count}
     </div>
-
+     <button onClick={increase}>
+      <em>
+        Increase
+      </em>
+     </button>
+     <button onClick={decrease}>
+      <em>
+        Decrease
+      </em>
+     </button>
+     <button onClick={setTo0}>
+      <em>
+        Set to 0
+        </em>
+     </button>
   </div>
  
 }
